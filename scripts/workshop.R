422c8f03073e08653c901cb0efa990cf544b638d
Liisa Kautto <liisakautto@Liisas-MacBook-Air.local>
2018-11-29 09:36:22 +1100

    commants

--- scripts/workshop.R
+++ scripts/workshop.R
@@ -2,3 +2,10 @@
 ## The csv file containing the data is in the 'data/' directory.
 ## If you want to save plots you created, place them in the 'figures/' directory.
 ## You can create additional R files in the 'scripts/' directory.
+library(tidyverse)
+
+interviews <- read_csv("data/SAFI_clean.csv",na="NULL")
+interviews
+# can be show as original data, use command  as.data.frame(interview)
+view(interviews)
+
