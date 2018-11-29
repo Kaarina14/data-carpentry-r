730ec9bc79b23020f95c94c50fc6e3202adf637e
Liisa Kautto <liisakautto@Liisas-MacBook-Air.local>
2018-11-29 13:19:14 +1100

    Select data

--- scripts/Dplyr.R
+++ scripts/Dplyr.R
@@ -1,1 +1,8 @@
 #dplyr and tidyr
+
+library(tidyverse)
+interviews <-read_scv("data/SAFI_clean.csv", na="NULL")
+select(interviews, village, no_membrs, years_liv)
+
+#choose columns villages to rooms
+select(interviews, village:rooms)
